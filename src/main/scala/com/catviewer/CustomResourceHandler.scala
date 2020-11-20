d1955055f153ff4138c4306ba7052d32a878a025
rmucha <rmucha@virtuslab.com>
2020-11-20 21:33:53 +0100

    Update build.gradle and refactor CustomResourceHandler

--- src/main/scala/com/catviewer/CustomResourceHandler.scala
+++ src/main/scala/com/catviewer/CustomResourceHandler.scala
@@ -9,74 +9,81 @@ import org.cef.misc.{IntRef, StringRef}
 import org.cef.network.{CefRequest, CefResponse}
 
 class CustomResourceHandler extends CefResourceHandler {
-  var connectionOption:  Option[JarURLConnection] = None
-  var inputStreamOption: Option[InputStream]      = _
-  override def processRequest(cefRequest: CefRequest, cefCallback: CefCallback): Boolean = {
+  var maybeConnection: Option[JarURLConnection] = None
+  var maybeInputStream: Option[InputStream] = None
+  override def processRequest(
+      cefRequest: CefRequest,
+      cefCallback: CefCallback
+  ): Boolean = {
     val urlOption = Option(cefRequest.getURL)
     urlOption match {
       case Some(processedUrl) =>
         val pathToResource = processedUrl.replace("http://myapp", "webview/")
-        val newUrl         = getClass.getClassLoader.getResource(pathToResource)
-        connectionOption = Some(newUrl.openConnection().asInstanceOf[JarURLConnection])
+        val newUrl = getClass.getClassLoader.getResource(pathToResource)
+        maybeConnection = Some(
+          newUrl.openConnection().asInstanceOf[JarURLConnection]
+        )
         cefCallback.Continue()
         true
       case None => false
     }
   }
 
-  override def getResponseHeaders(cefResponse: CefResponse, responseLength: IntRef, redirectUrl: StringRef): Unit = {
-    if (connectionOption.isDefined) {
-      try {
-        val connection = connectionOption.get
-        cefResponse.setMimeType(connection.getContentType)
-        if (connectionOption.get.getURL.toString.contains("css")) {
-          cefResponse.setMimeType("text/css")
-        } else {
-          if (connectionOption.get.getURL.toString.contains("js")) {
-            cefResponse.setMimeType("text/javascript")
+  override def getResponseHeaders(
+      cefResponse: CefResponse,
+      responseLength: IntRef,
+      redirectUrl: StringRef
+  ): Unit = {
+    maybeConnection match {
+      case None => cefResponse.setStatus(404)
+      case Some(connection) =>
+        try {
+          val url = connection.getURL.toString
+          url match {
+            case x if x.contains("css") => cefResponse.setMimeType("text/css")
+            case x if x.contains("js") =>
+              cefResponse.setMimeType("text/javascript")
+            case _ => cefResponse.setMimeType(connection.getContentType)
           }
+          maybeInputStream = Some(connection.getInputStream)
+          responseLength.set(maybeInputStream.get.available())
+          cefResponse.setStatus(200)
+        } catch {
+          case e: IOException =>
+            cefResponse.setError(CefLoadHandler.ErrorCode.ERR_FILE_NOT_FOUND)
+            cefResponse.setStatusText(e.getLocalizedMessage)
+            cefResponse.setStatus(404)
         }
-        inputStreamOption = Some(connection.getInputStream)
-        responseLength.set(inputStreamOption.get.available())
-        cefResponse.setStatus(200)
-      } catch {
-        case e: IOException =>
-          cefResponse.setError(CefLoadHandler.ErrorCode.ERR_FILE_NOT_FOUND)
-          cefResponse.setStatusText(e.getLocalizedMessage)
-          cefResponse.setStatus(404)
-      }
-    } else {
-      cefResponse.setStatus(404)
     }
   }
 
   override def readResponse(
-    dataOut:             Array[Byte],
-    designedBytesToRead: Int,
-    bytesRead:           IntRef,
-    callback:            CefCallback
+      dataOut: Array[Byte],
+      designedBytesToRead: Int,
+      bytesRead: IntRef,
+      callback: CefCallback
   ): Boolean = {
-    if (inputStreamOption.isDefined) {
-      val inputStream   = inputStreamOption.get
-      val availableSize = inputStream.available()
-      if (availableSize > 0) {
-        val bytesToRead           = Math.min(availableSize, designedBytesToRead)
-        val realNumberOfReadBytes = inputStream.read(dataOut, 0, bytesToRead)
-        bytesRead.set(realNumberOfReadBytes)
-        true
-      } else {
-        inputStreamOption.get.close()
-        connectionOption  = None
-        inputStreamOption = None
-        false
-      }
-    } else {
-      false
+    maybeInputStream match {
+      case None => false
+      case Some(inputStream) =>
+        val availableSize = inputStream.available()
+        if (availableSize > 0) {
+          val maxBytesToRead = Math.min(availableSize, designedBytesToRead)
+          val realNumberOfReadBytes =
+            inputStream.read(dataOut, 0, maxBytesToRead)
+          bytesRead.set(realNumberOfReadBytes)
+          true
+        } else {
+          inputStream.close()
+          maybeConnection = None
+          maybeInputStream = None
+          false
+        }
     }
   }
 
   override def cancel(): Unit = {
-    inputStreamOption = None
-    connectionOption  = None
+    maybeInputStream = None
+    maybeConnection = None
   }
 }
